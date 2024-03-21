0a49821b323955b51d22e99018a91c47dedbaa22
ryanc386 <ciolek98@students.rowan.edu>
2024-03-21 14:03:35 -0400

    Fixed environment and added basic LED code

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,17 @@
 #include <Arduino.h>
-
+#define LEDpin 25
 // put function declarations here:
-int myFunction(int, int);
+
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(LEDpin,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  digitalWrite(LEDpin,HIGH);
+  delay(1000);
+  digitalWrite(LEDpin,LOW);
+  delay(1000);
 }
